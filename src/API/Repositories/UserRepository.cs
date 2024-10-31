817d7d4841c9d549ab606e2479b0472e9766112b
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-10-31 16:39:25 +0700

    refactor: change UserRepository.cs to class instead record, and implement database layer

--- src/API/Repositories/UserRepository.cs
+++ src/API/Repositories/UserRepository.cs
@@ -1,7 +1,58 @@
-﻿namespace API.Repositories;
+﻿using API.Models.DTOs;
+using API.Models.Entities;
 
-public record UserRepository(
-    int Id, 
-    string Email, 
-    string Password
-    );
+namespace API.Repositories;
+
+
+/*
+ * TODO:
+ * Connect UserRepository to Users column
+ */
+public class UserRepository
+{
+
+    
+    public UserRepository()
+    {
+        
+    }
+    public string? TableName { get; set; }
+    
+    public async Task<User?> GetItemAsync(int id)
+    {
+        if (TableName == null) return null;
+        await Task.Delay(10);
+        var user = new User
+        {
+            Id = 1,
+            Email = "Hello World",
+            Username = "Hello World",
+            Password = "Hello World",
+            Fullname = "Hello World",
+            CreatedAt = default,
+            LastModified = default
+        };
+        return user;
+    }
+
+    public Task<List<User>?> GetAllItemsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> CreateItemAsync(UserDetailDto model)
+    {
+        
+        throw new NotImplementedException();
+    }
+
+    public Task<User> UpdateItemAsync(UserDetailDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<(bool status, string message)> DeleteItemAsync(int Id)
+    {
+        throw new NotImplementedException();
+    }
+}
