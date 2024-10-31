a612055ce821c66231960999d185e2ec8e9a1889
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-01 00:59:39 +0700

    feat: add Data Annotation

--- src/API/Models/DTOs/UserDetailDto.cs
+++ src/API/Models/DTOs/UserDetailDto.cs
@@ -1,9 +1,15 @@
-﻿namespace API.Models.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.DTOs;
 
 public record UserDetailDto(
     int? Id,
+    [Required(ErrorMessage = "Email is Required")]
     string Email,
+    [Required(ErrorMessage = "Username is Required")]
     string Username,
+    [Required(ErrorMessage = "Password is Required")]
     string Password,
+    [Required(ErrorMessage = "Full name is Required")]
     string Fullname
     );
