2484dc8b7b4ddc64d95299f1d6a784b9f1e2da1f
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-10-31 16:38:57 +0700

    feat: initial add User entity

---
+++ src/API/Models/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.Entities;
+
+public class User
+{
+    
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Email is required")]
+    public required string Email { get; set; }
+    
+    [Required(ErrorMessage = "Username is required")]
+    public required string Username { get; set; }
+    
+    [Required(ErrorMessage = "Password is required")]
+    public required string Password { get; set; }
+    
+    [Required(ErrorMessage = "Name is required")]
+    public required string Fullname { get; set; }
+    
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime LastModified { get; set; } = DateTime.Now;    
+}
