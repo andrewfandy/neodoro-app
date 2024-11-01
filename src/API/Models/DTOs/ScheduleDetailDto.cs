d01ea4413ab1078a3dacb876ef4391bce215bfa9
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-01 14:01:25 +0700

    feat: add ScheduleDetailDto.cs

---
+++ src/API/Models/DTOs/ScheduleDetailDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.DTOs;
+
+public record ScheduleDetailDto(
+    
+    int Id,
+    
+    [Required(ErrorMessage = "UserId is required")]
+    int UserId,
+
+    [Required(ErrorMessage = "TimerSessionId is required")]
+    int TimerSessionId,
+
+    [Required(ErrorMessage = "Schedule name is required")]
+    string Name,
+    
+    [Required(ErrorMessage = "Complete status is required")]
+    bool IsComplete,
+    
+    [Required(ErrorMessage = "Active status is required")]
+    bool IsActive,
+    
+    [Required(ErrorMessage = "Pause status is required")]
+    bool IsPause,
+    
+    DateTime LastModified
+    
+    );
