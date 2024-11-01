cf12d0ecf5413f28d846ff6fe9145c00ad02681f
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-01 14:01:14 +0700

    feat: add ScheduleRepository.cs

---
+++ src/API/Repositories/ScheduleRepository.cs
@@ -0,0 +1,44 @@
+﻿using API.Models.DTOs;
+using API.Models.Entities;
+
+namespace API.Repositories;
+
+/*
+ * TODO:
+ * Connect ScheduleRepository to Schedule column
+ */
+public class ScheduleRepository
+{
+        
+    public ScheduleRepository()
+    {
+        
+    }
+    public string? TableName { get; set; }
+    
+    public async Task<Schedule?> GetItemAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Schedule>?> GetAllItemsAsync()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task<Schedule> CreateItemAsync(ScheduleDetailDto model)
+    {
+        
+        throw new NotImplementedException();
+    }
+
+    public Task<Schedule> UpdateItemAsync(ScheduleDetailDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<(bool status, string message)> DeleteItemAsync(int Id)
+    {
+        throw new NotImplementedException();
+    }
+}
