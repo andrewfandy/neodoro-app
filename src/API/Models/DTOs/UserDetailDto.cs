6893afbb365a0d855f5ae885abb9a8fba59806f6
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-01 14:00:17 +0700

    style: add space between props

--- src/API/Models/DTOs/UserDetailDto.cs
+++ src/API/Models/DTOs/UserDetailDto.cs
@@ -4,12 +4,17 @@ namespace API.Models.DTOs;
 
 public record UserDetailDto(
     int? Id,
+    
     [Required(ErrorMessage = "Email is Required")]
     string Email,
+   
     [Required(ErrorMessage = "Username is Required")]
     string Username,
+    
     [Required(ErrorMessage = "Password is Required")]
     string Password,
+    
     [Required(ErrorMessage = "Full name is Required")]
     string Fullname
+    
     );
