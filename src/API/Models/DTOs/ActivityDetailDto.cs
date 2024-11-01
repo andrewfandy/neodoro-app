a1170eae1bd4f042e23593d628ea755073b2b52f
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-01 14:02:21 +0700

    style: add space between props

--- src/API/Models/DTOs/ActivityDetailDto.cs
+++ src/API/Models/DTOs/ActivityDetailDto.cs
@@ -4,13 +4,18 @@ namespace API.Models.DTOs;
 
 public record ActivityDetailDto(
     int? Id,
+    
     [Required(ErrorMessage = "Schedule reference is Required")]
     int ScheduleId,
+    
     [Required(ErrorMessage = "Activity name is required")]
     string Name,
+    
     [Required(ErrorMessage = "Activity complete status is required")]
     bool IsComplete,
+    
     DateTime CreatedAt,
+    
     DateTime LastModified
 );
     
