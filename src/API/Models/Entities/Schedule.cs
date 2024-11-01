5f08208ed0ba6c0ab81fa81aed54641cdfea1096
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-01 14:01:35 +0700

    feat: add Schedule.cs

---
+++ src/API/Models/Entities/Schedule.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Models.Entities;
+
+public class Schedule
+{
+    public int Id { get; set; }
+    
+    public required User User { get; set; }
+    
+    public required TimerSession TimerSession { get; set; } // one to one with TimerSession
+    
+    public required string Name { get; set; }
+
+    public bool IsComplete { get; set; } = false;
+
+    public bool IsActive { get; set; } = false;
+
+    public bool IsPause { get; set; } = true;
+    
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime LastModified { get; set; } = DateTime.Now;
+}
