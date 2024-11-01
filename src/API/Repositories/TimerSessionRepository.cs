6e1426e58816a71fa7a3c2a8ec5dd234ec6039dd
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-01 14:00:35 +0700

    feat: add TimerSessionRepository.cs

---
+++ src/API/Repositories/TimerSessionRepository.cs
@@ -0,0 +1,42 @@
+﻿using API.Models.DTOs;
+using API.Models.Entities;
+
+namespace API.Repositories;
+
+/*
+ * TODO:
+ * Connect TimerSessionRepository to TimerSession column
+ */
+public class TimerSessionRepository
+{
+    public TimerSessionRepository()
+    {
+        
+    }
+    public string? TableName { get; set; }
+    
+    public async Task<TimerSession?> GetItemAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<TimerSession>?> GetAllItemsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TimerSession> CreateItemAsync(TimerSessionDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TimerSession> UpdateItemAsync(TimerSessionDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<(bool status, string message)> DeleteItemAsync(int Id)
+    {
+        throw new NotImplementedException();
+    }
+}
