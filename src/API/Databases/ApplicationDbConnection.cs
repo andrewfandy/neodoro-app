6d76f3c13775d0d3953ec5f167a4f2929bf10b9f
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-03 21:48:46 +0700

    feat: add validation if table exists

--- src/API/Databases/ApplicationDbConnection.cs
+++ src/API/Databases/ApplicationDbConnection.cs
@@ -1,4 +1,8 @@
 using System.Data;
+using System.Text;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Databases;
 
@@ -12,4 +16,39 @@ public class ApplicationDbConnection
                            ?? throw new NullReferenceException($"ConnectionString is null, Current Environment: {environment}");
     }
     
+    public async Task<bool> CheckTableIfExists(string? schema,string table)
+    {
+        await using (var sqlConnection = new SqlConnection(ConnectionString))
+        {
+            var sb = new StringBuilder();
+            sb.Append("SELECT * FROM sys.tables t\n");
+            if (!schema.IsNullOrEmpty())
+            {
+                sb.Append("JOIN sys.schemas s ON (s.schema_id = t.schema_id)\n");
+                sb.Append("WHERE s.name=@Schema AND t.name = @Table");
+            }
+            else
+            {
+                sb.Append("WHERE t.name = @Table");
+
+            }
+
+            var sql = sb.ToString();
+            var tables = await sqlConnection.QueryAsync(
+                sql,
+                new
+                {
+                    Schema = schema,
+                    Table = table
+                }
+            );
+
+            return tables.Any();
+        }
+    }
+
+    public async Task<bool> CheckTableIfExists(string table)
+    {
+        return await CheckTableIfExists(null, table);
+    }
 }
