733db32675f94be177e722c4cbfc94c4772f6f16
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-11-10 21:37:20 +0700

    refactor: nullable on Message and Data

--- src/API/Services/Common/Result.cs
+++ src/API/Services/Common/Result.cs
@@ -3,6 +3,6 @@ namespace API.Services.Common;
 public class Result<T>
 {
     public bool IsSuccess { get; set; }
-    public required string Message { get; set; }
-    public required T Data { get; set;} 
+    public string? Message { get; set; }
+    public T? Data { get; set;} 
 }
